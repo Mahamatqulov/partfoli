d4796fccb9b0e6136ec85490966b314965905940
bahrom <baxrom@email.com>
2025-02-28 15:21:59 +0500

    refactor: update NotFound component layout and remove unused image

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,12 +1,10 @@
-import Image from "next/image";
-import NotFoundImage from "/public/404.png";
 import Link from "next/link";
 import { buttonVariants } from "@/components/ui/button";
 
 export default function NotFound() {
   return (
     <div className="">
-      <div className="container mx-auto px-5 max-w-screen-sm flex flex-col-reverse items-center gap-5 sm:flex-row justify-between">
+      <div className="container mx-auto  px-5 max-w-screen-sm flex mt-[150px] flex-col-reverse items-center gap-5 sm:flex-row">
         <div>
           <h2 className="text-5xl mb-2 animate-slide-top">404</h2>
           <p className="mb-3 animate-slide-top delay-300">
@@ -21,13 +19,6 @@ export default function NotFound() {
             Bosh sahifaga qaytish
           </Link>
         </div>
-        <Image
-          className="w-[150px] object-cover animate-slide-top sm:delay-900"
-          src={NotFoundImage}
-          width={150}
-          height={150}
-          alt="404"
-        />
       </div>
     </div>
   );
